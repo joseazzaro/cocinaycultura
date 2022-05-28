c028c511b73ca85680e3de2cade42e7a407e6b06
joseazzaro <84251779+joseazzaro@users.noreply.github.com>
2022-05-28 17:49:00 -0300

    Update Enviar_mail.php

--- CyC/Enviar_mail.php
+++ CyC/Enviar_mail.php
@@ -1,8 +1,8 @@
 <?php
 
-    $nombre_mail=#_POST["name"];
-    $direccion_mail=#_POST["mail"];
-    $mensaje_mail=#_POST["message"];
+    $nombre_mail=$_POST["name"];
+    $direccion_mail=$_POST["mail"];
+    $mensaje_mail=$_POST["message"];
 
     $headers="MIME-Version: 1.0\r\n";
     $headers.="Content-type: text/html; charset=iso-8859-1\r\n";
